c71642fc325d24589d15e62288a210265dc510dc
xxzytu <Riddhiman.reddy@gmail.com>
2025-05-19 14:50:44 +0530

    Ui for login.tsx

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -4,7 +4,7 @@ import { useRouter } from 'expo-router';
 import { View, Text, TextInput, TouchableOpacity, ActivityIndicator, Alert } from 'react-native';
 
 export default function Login() {
-    const [email. setEmail] = useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [loading, setLoading] = useState(false);
     const { signIn } = useAuth();
@@ -13,7 +13,7 @@ export default function Login() {
     const handleLogin = async () => {
         if (!email || !password) {
             Alert.alert('please fill all fields');
-            return:
+            return;
         }
 
         setLoading(true);
@@ -29,5 +29,43 @@ export default function Login() {
         }
     };
 
-    
+    return (
+       <View className="flex-1 justify-center items-center bg-white">
+        <Text className="text-3xl font-bold mb-4">Namaste, welcome back!</Text>
+        <Text className="text-gray-100 mb-8">Login to continue</Text>
+
+        <TextInput 
+         className="border-gray-300 border-3 rounded-lg p-2 mb-4"
+         placeholder='E-mail'
+         value={email}
+         onChangeText={setEmail}
+         keyboardType="email-address"
+         autoCapitalize="none"
+         />
+        
+        <TextInput className="border-gray-300 border-3 rounded-lg p-2 mb-4"
+         placeholder="password"
+         value={password}
+         onChangeText={setPassword}
+         secureTextEntry />
+
+         <TouchableOpacity className="bg-blue-400 rounded-xl py-3 items-center max-w-md self-center w-full"
+         onPress={handleLogin}
+         disabled={loading}>
+
+            {loading ? (
+                <ActivityIndicator color="white" />
+            ) : (
+                <Text className="text-white font-bold text-center">Log In</Text>
+            )}
+         </TouchableOpacity>
+
+         <TouchableOpacity className="items-center"
+          onPress={() => router.push("/(auth)/register")}
+          >
+            <Text className="text-blue-400">Don't have an account? Sign up</Text>
+          </TouchableOpacity>
+
+         </View> 
+    )
 }
