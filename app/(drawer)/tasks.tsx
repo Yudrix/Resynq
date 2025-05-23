0efff60c432112a824123f7a3cc31c1c32381ccc
xxzytu <Riddhiman.reddy@gmail.com>
2025-05-23 07:47:18 +0530

    halfway through task functionality

---
+++ app/(drawer)/tasks.tsx
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity, ActivityIndicator, FlatList, TextInput } from 'react-native';
+import { useAuth } from '../../context/auth';
+import { Task } from '../../types';
+
+export default function TasksScreen() {
+    const { user } = useAuth();
+    const [tasks, setTasks] = useState<Task[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [showAddTask, setShowAddTask] = useState(false);
+    const [newTask, setNewTask] = useState({
+        title: '',
+        description: '',
+        dueDate: new Date(),
+        priority: 'Urgent and important' as 'Not urgent but important' | 'Not urgent and fast' | 'Urgent and important' | 'Upcoming event' | 'Important but not urgent',
+        category: '',
+    });
+    const [showDatePicker, setShowDatePicker] = useState(false);
+    const insets = useSafeAreaInsets();
+
+    useEffect(() => {
+        if (!user) return;
+
+        const q = query(
+            collection(db, 'tasks'),
+            where('userId', '==', user.uid),   //To be changed to username or custom display
+            orderBy('createdAt', 'asc')
+        );
+
+        const unsubscribe = onSnapshot(q, (snapshot) => {
+            const taskList: Task[] = [];
+            snapshot.forEach((doc) => {
+                const data = doc.data();
+                taskList.push({
+                    id: doc.id,
+                    title: data.title,
+                    description: data.description || '',
+                    dueDate: data.dueDate.toDate(),
+                    completed: data.completed,
+                    priority: data.priority,
+                    category: data.category || '',
+                    userId: data.userId,
+                    createdAt: data.createdAt.toDate(),
+                });
+            });
+            setTasks(taskList);
+            setLoading(false);
+        });
+        return unsubscribe;
+    }, [user]);
+
+    //Add new task not modal but it's fine *subject to change*
+    const handleAddTask = async () => {
+        if (!user || !newTask.title.trim()) return;
+
+        try {
+            await addDoc(collection(db, 'tasks'), {
+                title: newTask.title,
+                description: newTask.description,
+                dueDate: Timestamp.fromDate(newTask.dueDate),
+                completed: false,
+                priority: newTask.priority,
+                category: newTask.category,
+                userId: user.uid,
+                createdAt: Timestamp.fromDate(new Date())
+            });
+
+            setNewTask({
+                
+            })
+        }
+    }
+}
